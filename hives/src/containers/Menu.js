f1ebb6cd7f582179b2727178644fa032f3073cd2
Unknown <clara.pompeu96@gmail.com>
2018-05-22 22:21:15 -0300

    Auto stash before merge of "master" and "origin/master"

--- hives/src/containers/Menu.js
+++ hives/src/containers/Menu.js
@@ -1,20 +1,46 @@
 import React from "react";
+import { Menu } from 'semantic-ui-react'
 import {
   Link,
 } from "react-router-dom";
 
 import { AuthButton } from "../App";
 
-export default class Menu extends React.Component {
+export default class MainMenu extends React.Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name})
+
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div>
-        <ul>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/chat">Chat</Link></li>
-          <li><AuthButton /></li>
-        </ul>
-      </div>
+      <Menu>
+        <Menu.Item
+          name = 'LogOut'
+          active = { activeItem === 'LogOut' }
+          onClick = { this.handleItemClick }
+        >
+          <AuthButton />
+        </Menu.Item>
+
+        <Menu.Item
+          name = 'Dashboard'
+          active = { activeItem === 'Dashboard' }
+          onClick = { this.handleItemClick }
+        >
+          <Link to="/dashboard">Dashboard</Link>
+        </Menu.Item>
+
+        <Menu.Item
+          name = 'Chat'
+          active = { activeItem === 'Chat' }
+          onClick= { this.handleItemClick }
+        >
+          <Link to="/chat">Chat</Link>
+        </Menu.Item>
+      </Menu>
     )
   }
 }
+

b6b51d9c03d53cf11d35d02c02d3a9fd6d8417a2
Leonardo H. Batista <leohbatista@users.noreply.github.com>
2018-05-22 21:34:58 -0300

    Added first auth

---
+++ hives/src/containers/Chat.js
@@ -0,0 +1,18 @@
+import React from "react";
+/*import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";*/
+
+export default class Chat extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Chat</h3>
+      </div>
+    )
+  }
+}
---
+++ hives/src/containers/Dashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+/*import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";*/
+
+export default class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Dashboard</h3>
+      </div>
+    )
+  }
+}

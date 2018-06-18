a5274bc60460cce2595f29d8cd1512e05d4227c8
marinagui <marinagui10@hotmail.com>
2018-06-18 18:19:48 -0300

    rotas sobre Vaga

--- back/router.js
+++ back/router.js
@@ -1,11 +1,12 @@
 var usuario = require('./usuario')
-
+var vaga = require('./vaga')
 
 // Define os endpoints, chamando as funções declaradas nos controllers
 var defineRoutes = function (app) {
 	app.get('/usuario', usuario.get);
 	app.post('/usuario/login', usuario.checkLogin);
-
+	app.get('/vaga', vaga.get);
+	app.get('/vaga/empresa/:idEmpresa', vaga.getPorEmpresa);
 }
 
 module.exports = {

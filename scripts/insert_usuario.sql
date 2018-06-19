48276602f689d790278a26eb55f37d416f7e6464
Rebecca Moreira Messias <rebecca.messias@outlook.com>
2018-06-19 17:47:09 -0300

    Added scripts to populate the database

---
+++ scripts/insert_usuario.sql
@@ -0,0 +1,5 @@
+INSERT INTO Usuario (login, senha, nome, tipoUsuario) VALUES ('user01', 'pwd01', 'Usuario 01', 0);
+INSERT INTO Usuario (login, senha, nome, tipoUsuario) VALUES ('user02', 'pwd02', 'Usuario 02', 1);
+INSERT INTO Usuario (login, senha, nome, tipoUsuario) VALUES ('user03', 'pwd03', 'Usuario 03', 0);
+INSERT INTO Usuario (login, senha, nome, tipoUsuario) VALUES ('user04', 'pwd04', 'Usuario 04', 1);
+INSERT INTO Usuario (login, senha, nome, tipoUsuario) VALUES ('user05', 'pwd05', 'Usuario 05', 0);

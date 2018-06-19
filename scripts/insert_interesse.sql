7b74663072b97de0bab8a6bf93a83054144828cb
Rebecca Moreira Messias <rebecca.messias@outlook.com>
2018-06-19 20:55:28 -0300

    Fixed 'interesse' script

--- scripts/insert_interesse.sql
+++ scripts/insert_interesse.sql
@@ -1,7 +1,7 @@
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Rebecca Messias', 'rebecca@gmail.com', 1);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Marina Guimarães', 'marina@gmail.com', 6);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Leonardo Batista', 'leonardo@gmail.com', 11);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('André Soranzo', 'andre@gmail.com', 16);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Clara Carneiro', 'clara@gmail.com', 21);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Leandro Araújo', 'leandro@gmail.com', 26);
-INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('César Bueno', 'cesar@gmail.com', 31);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Rebecca Messias', 'rebecca@gmail.com', '991321272', 1);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Marina Guimarães', 'marina@gmail.com', '991321273', 6);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Leonardo Batista', 'leonardo@gmail.com', '991321274', 11);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('André Soranzo', 'andre@gmail.com', '991321275', 16);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Clara Carneiro', 'clara@gmail.com', '991321276', 21);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('Leandro Araújo', 'leandro@gmail.com', '991321277', 26);
+INSERT INTO Interesse (nomeInteressado, email, telefone, idVaga) VALUES ('César Bueno', 'cesar@gmail.com', '991321278', 31);

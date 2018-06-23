e4a0977b6711b17a1433038425fb983b924d9097
Cesar Bueno <barddes@gmail.com>
2018-06-23 17:08:29 -0300

    Chat 100% da tela

--- hives/src/containers/Chat.js
+++ hives/src/containers/Chat.js
@@ -12,7 +12,7 @@ import ChatWindow from '../containers/ChatWindow';
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { contacts : [] };
+    this.state = { contacts : [{}] };
   }
 
   componentDidMount(){
@@ -29,8 +29,8 @@ export default class Chat extends React.Component {
   render() {
     return (
       <Container fluid style={{ margin: '1em', backgroundColor: '#dddddd' }}>
-        <Grid>
-          <Grid.Column width={3}>
+        <Grid style={{ height: '100vh' }}>
+          <Grid.Column width={3} style={{ height: '100%' }}>
             <ContactList contacts={this.state.contacts}/>
           </Grid.Column>
           <Grid.Column width={13} style={{ paddingLeft: '1%', paddingRight: '2.5%' }}>
